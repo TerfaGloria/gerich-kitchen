d7918f5c3368d3e4c0ca8a38dd9788c16f59b2fe
TerfaGloria <terfagloria455@gmail.com>
2025-04-11 21:22:56 +0100

    obile view update

--- script/script.js
+++ script/script.js
@@ -1,31 +1,56 @@
 " use strict";
 
 
-const closeIcon=document.querySelector(".close-icon")
-const menuIcon=document.querySelector(".menu-icon")
-const homePage=document.querySelector('.home-page')
-const body=document.querySelector("body")
+// const closeIcon = document.querySelector(".close-icon");
+// const menuIcon = document.querySelector(".menu-icon");
+// const sideBar = document.querySelector('.sidebar');
+// const overLay=document.querySelector('.overlay')
 
+// menuIcon.addEventListener('click', () => {
+//   console.log('Menu icon clicked');
+//   sideBar.classList.remove('hidden');
+//   overLay.classList.remove('hidden')
+//   closeIcon.classList.remove('hidden');
+//   menuIcon.classList.add('hidden');
+// });
 
-// Add event listeners to the menu icon and close icon
+// closeIcon.addEventListener('click', () => {
+//   console.log('Close icon clicked');
+//   sideBar.classList.add('hidden');
+//   overLay.classList.add('hidden')
+//   closeIcon.classList.add('hidden');
+//   menuIcon.classList.remove('hidden');
+// });
+
+// overLay.addEventListener("click",()=>{
+//   document.body.classList.add
+// })
+const closeIcon = document.querySelector(".close-icon");
+const menuIcon = document.querySelector(".menu-icon");
+const sideBar = document.querySelector('.sidebar');
+const overLay = document.querySelector('.overlay');
+
 menuIcon.addEventListener('click', () => {
-    // Show the ul element
-    homePage.classList.remove('hidden');
-    body.classList.add("blur");
-    // Show the close icon
-    closeIcon.classList.remove('hidden');
-    // Hide the menu icon
-    menuIcon.classList.add('hidden');
-  });
-  
-  closeIcon.addEventListener('click', () => {
-    // Hide the ul element
-    homePage.classList.add('hidden');
-    body.classList.remove('blur');
-    // Hide the close icon
-    closeIcon.classList.add('hidden');
-    // Show the menu icon
-    menuIcon.classList.remove('hidden');
-  });
-  
+  console.log('Menu icon clicked');
+  sideBar.classList.remove('hidden');
+  // overLay.classList.add('hidden'); 
+  closeIcon.classList.remove('hidden');
+  menuIcon.classList.add('hidden');
+});
+
+closeIcon.addEventListener('click', () => {
+  console.log('Close icon clicked');
+  sideBar.classList.add('hidden');
+  // overLay.classList.remove('hidden'); 
+  closeIcon.classList.add('hidden');
+  menuIcon.classList.remove('hidden');
+});
+
+
+
+
+
+
+
+
   
